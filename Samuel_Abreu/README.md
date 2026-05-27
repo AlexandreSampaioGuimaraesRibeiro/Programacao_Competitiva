449d8b50a815c58b38e0830ced04454f2937fc7d
Samuel Abreu Silva Bispo <Samucabispo2202@gmail.com>
2026-05-26 23:12:12 -0300

    alteração para os sites aceitarem

--- Samuel_Abreu/README.md
+++ Samuel_Abreu/README.md
@@ -4,4 +4,6 @@
 Samuel Abreu
 
 ## Descrição
-Pasta destinada aos exercícios e atividades de programação competitiva da disciplina de Computabilidade.
+Pasta destinada aos exercícios e atividades de programação competitiva da disciplina de Computabilidade das seguintes atividades:
+# https://leetcode.com/problems/powx-n/description/ (Pow (x, n))
+# https://www.spoj.com/problems/PON/ (PON - Prime or Not)
--- Samuel_Abreu/primo.py
+++ Samuel_Abreu/primo.py
@@ -1,26 +1,21 @@
-lista = []
-contador = 0
-t = int(input("digite a quantidade de números que quer testar:"))
-if not t <= 500:
-    print ("a quantidade de números deve ser menor que 500")
-else:
-    while contador < t: 
-        n = int(input("digite aqui o número que quer testar se é primo ou não: "))
-        lista.append(n)
-        contador += 1
-for num in lista:
-    contador_divisor = 0
-    for i in range (1,num+1):
-        if num%i == 0:
-            contador_divisor += 1
-for num in lista:
-    print (num)
-for num in lista:
-    contador_divisor = 0
-    for i in range (1,num+1):
-        if num%i == 0:
-            contador_divisor += 1
-    if contador_divisor > 2:
-        print ("NÃO")
-    elif contador_divisor == 2:
-        print ("SIM")
+import math
+
+def eh_primo(n):
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for i in range(3, int(math.isqrt(n)) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    if eh_primo(n):
+        print("YES")
+    else:
+        print("NO")
