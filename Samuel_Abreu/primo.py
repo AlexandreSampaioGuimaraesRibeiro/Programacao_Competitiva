a901bc2f82f76fa115d0ffc4285a261ec7d8c657
Samuel Abreu Silva de Oliveira Bispo <1661128@sga.pucminas.br>
2026-05-28 19:06:15 -0300

    breve correção na parte do samuel bispo

--- Samuel_Abreu/primo.py
+++ Samuel_Abreu/primo.py
@@ -1,41 +1,45 @@
-lista = []
-contador = 0
+def miller_rabin(n, a):
+    if n % a == 0:
+        return n == a
+
+    r = 0
+    d = n - 1
+    while d % 2 == 0:
+        r += 1
+        d = d // 2
+    
+    x = pow(a, d, n)
+
+    if x == 1 or x == n - 1:
+        return True
+
+    for _ in range(r - 1):
+        x = pow(x, 2, n)
+        if x == n - 1:
+            return True
+
+    return False
+
+
+def e_primo(n):
+    if n < 2:
+        return False
+
+    bases = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37]
+
+    for a in bases:
+        if not miller_rabin(n, a):
+            return False
+
+    return True
+
 
 t = int(input())
 
-while contador < t:
-
+for _ in range(t):
     n = int(input())
 
-    lista.append(n)
-
-    contador += 1
-
-
-for num in lista:
-
-    if num == 2:
-        print("YES")
-
-    elif num < 2 or num % 2 == 0:
-        print("NO")
-
+    if e_primo(n):
+        print("SIM")
     else:
-
-        primo = True
-
-        i = 3
-
-        while i * i <= num:
-
-            if num % i == 0:
-                primo = False
-                break
-
-            i += 2
-
-        if primo:
-            print("YES")
-
-        else:
-            print("NO")
+        print("NÃO")
