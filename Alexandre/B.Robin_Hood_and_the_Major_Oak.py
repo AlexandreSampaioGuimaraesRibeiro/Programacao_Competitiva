90b4091cf3c7982675f112eee0b11331a08f1348
AlexandreSampaioGuimaraesRibeiro <lexandre.guimaraes@gmail.com>
2026-06-14 18:26:46 -0300

    FINALIZANDO

--- Alexandre/B.Robin_Hood_and_the_Major_Oak.py
+++ Alexandre/B.Robin_Hood_and_the_Major_Oak.py
@@ -1,31 +1,10 @@
-def quantidade(matriz,v):
-    if matriz[v][1] == 1:
-        return matriz[v][0]**matriz[v][0]
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().split())
+    l = max(1, n - k + 1)
+    r = n
+    qtd_impares = (r + 1) // 2 - l // 2
+    if qtd_impares % 2 == 0:
+        print("YES")
     else:
-        folhas=0
-        t=matriz[v][0]
-        f=matriz[v][1]
-        for i in range(0,len(matriz)):
-            if matriz[i][1]+t-1 >=f and matriz[i] != matriz[v]:
-                folhas+=matriz[i][0]**matriz[i][0]
-    return folhas
-
-
-
-
-
-
-t=int(input(""))
-f=0
-testes=[]
-for i in range(t):
-    anos=[]
-    anos.append(int(input("")))
-    anos.append(int(input("")))
-    testes.append(anos)
-    r=quantidade(testes,i)
-    if r%2==0:
-        print("True")
-    else:
-        print("False")
-
+        print("NO")
