e89e9154a81c93d7bbe6d8d3c4ae4b108fb35f02
Samuel Abreu Silva Bispo <Samucabispo2202@gmail.com>
2026-06-14 18:23:06 -0300

    correção do yes e no

--- Samuel_Abreu/primo.py
+++ Samuel_Abreu/primo.py
@@ -4,17 +4,19 @@ def miller_rabin(n, a):
 
     r = 0
     d = n - 1
+
     while d % 2 == 0:
         r += 1
         d = d // 2
-    
+
     x = pow(a, d, n)
 
     if x == 1 or x == n - 1:
         return True
 
-    for _ in range(r - 1):
+    for z in range(r - 1):
         x = pow(x, 2, n)
+
         if x == n - 1:
             return True
 
@@ -40,6 +42,6 @@ for i in range(t):
     n = int(input())
 
     if e_primo(n):
-        print("SIM")
+        print("YES")
     else:
-        print("NÃO")
+        print("NO")
